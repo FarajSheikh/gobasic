64190ce9b8da971806f0dd0f27eaa02af7345b12
Faraj Sheikh <farajsheikh111@gmail.com>
2025-06-30 15:41:33 +0530

    ifelseladder

---
+++ ifelseladder/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	v1 := 700
+
+	if v1 == 100 {
+		fmt.Println("value of v1 is 100")
+	} else if v1 == 200 {
+		fmt.Println("value of a is 20")
+	} else if v1 == 300 {
+		fmt.Println("value of a is 300")
+	} else {
+		fmt.Println("none of the value is matching")
+	}
+}
